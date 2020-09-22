35dce69315c847bda68f4cd0ec3a6ca9178e08c3
SergeChurkin-ca <sergechurkin@yahoo.com>
2020-09-22 16:26:37 -0400

    fixed inclusion of routerd page in nav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import logo from "../../logo.svg";
 
 import AgentsPage from "../agentsPage/App";
@@ -7,27 +7,29 @@ import "./navbar.scss";
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <ul className="nav-links">
-        <li>
-          <img src={logo} alt="bee logo" className="logo" />
-          <h4> tour_Bees / </h4>
-        </li>
+    <Router>
+      <nav className="navbar">
+        <ul className="nav-links">
+          <li>
+            <img src={logo} alt="bee logo" className="logo" />
+            <h4> tour_Bees / </h4>
+          </li>
 
-        <li>
-          <Link to="/">Home</Link>
-        </li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
 
-        <li>
-          <Link to="/agentsPage/" className="agents-page">
-            Manage Inventory
-          </Link>
-        </li>
-      </ul>
+          <li>
+            <Link to="/agentsPage/" className="agents-page">
+              Manage Inventory
+            </Link>
+          </li>
+        </ul>
+      </nav>
       <Switch>
         <Route exact path="/agentsPage/" component={AgentsPage} />
       </Switch>
-    </nav>
+    </Router>
   );
 }
 
