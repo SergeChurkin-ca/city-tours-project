3d1605147a1ba318cc54f641dac8188a5db13621
SergeChurkin-ca <sergechurkin@yahoo.com>
2020-09-22 21:21:18 -0400

    Updates

---
+++ precache-manifest.070155e9fedd6184bdf57659648c241f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c843b432714698286ecccf49e46d20b6",
+    "url": "/city-tours-project/index.html"
+  },
+  {
+    "revision": "2f2ee81c40dec353a972",
+    "url": "/city-tours-project/static/css/main.823a6ba4.chunk.css"
+  },
+  {
+    "revision": "66f2c67c639a95052ec0",
+    "url": "/city-tours-project/static/js/2.fffff7cd.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/city-tours-project/static/js/2.fffff7cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f2ee81c40dec353a972",
+    "url": "/city-tours-project/static/js/main.0940ad9f.chunk.js"
+  },
+  {
+    "revision": "b096adb4dfd86bb160d4",
+    "url": "/city-tours-project/static/js/runtime-main.a8072623.js"
+  },
+  {
+    "revision": "a5527ad1c0888a95c03007fbfc5b7ae3",
+    "url": "/city-tours-project/static/media/logo.a5527ad1.svg"
+  }
+]);
