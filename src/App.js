52ba0f0cd29adba48564ba260832a670226aa71d
SergeChurkin-ca <sergechurkin@yahoo.com>
2020-09-22 22:54:29 -0400

    router bug

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import React, { Component } from "react";
+
 import "./App.scss";
 import Navbar from "./components/Navbar/Navbar";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  BrowserRouter,
-} from "react-router-dom";
 
 import TourList from "./components/TourList/index";
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <Router>
-          <Navbar />
-        </Router>
-        <React.Fragment>
-          <TourList />
-        </React.Fragment>
-      </BrowserRouter>
+      <React.Fragment>
+        <Navbar />
+        <TourList />
+      </React.Fragment>
     );
   }
 }
